0dd73aeb9b6f84f36e77f341584d0971e213c6d8
Jurjen Vos <jurjen.vos@webstores.nl>
2020-01-02 15:36:23 +0100

    feat (work): infiniteScroll implementation

--- web-src/assets/js/custom/masonry.js
+++ web-src/assets/js/custom/masonry.js
@@ -1,144 +1,81 @@
 import Masonry from "masonry-layout";
+import InfiniteScroll from "infinite-scroll";
+import CalculateVideoHeight from "./CalculateVideoHeight";
 import imagesLoaded from "imagesloaded";
-import CalculateVideoHeight from './CalculateVideoHeight';
+import Player from "@vimeo/player";
+
+InfiniteScroll.imagesLoaded = imagesLoaded;
 
 class MasonryGrid {
     constructor(el) {
         this.DOM = { el: el };
         this.DOM.videos = Array.from(this.DOM.el.querySelectorAll("[data-vimeo-player]"));
+
         this.grid = new Masonry(this.DOM.el, {
-            itemSelector: ".row__item",
+            itemSelector: "none",
             columnWidth: ".row__item",
-            percentPosition: true
+            percentPosition: true,
+            visibleStyle: { transform: "translateY(0)", opacity: 1 },
+            hiddenStyle: { transform: "translateY(100px)", opacity: 0 }
         });
 
         this.initMasonry();
     }
     initMasonry = () => {
-        new imagesLoaded(this.DOM.el, () => {
-            this.grid.layout();
+        new CalculateVideoHeight(this.DOM.videos, this.grid);
+
+        imagesLoaded(this.DOM.el, () => {
+            const gridItems = this.DOM.el.querySelectorAll(".row__item");
+            this.grid.options.itemSelector = ".row__item";
+            this.grid.appended(gridItems);
         });
 
-        new CalculateVideoHeight(this.DOM.videos, this.grid)
+        const infiniteScroll = new InfiniteScroll(this.DOM.el, {
+            path: function() {
+                const pageType = this.element.dataset.pageType;
+                const totalPages = this.element.dataset.totalPages;
+
+                if (this.loadCount < totalPages) {
+                    if (this.loadCount === 0) this.loadCount = 1; // Start at 1
+
+                    const nextIndex = this.loadCount + 1;
+                    return `${pageType}/p${nextIndex}`;
+                }
+            },
+            append: ".row__item",
+            history: false,
+            outlayer: this.grid
+        });
+
+        infiniteScroll.on("append", (response, path, items) => {
+            items.forEach(item => {
+                const video = item.querySelector("[data-vimeo-player]");
+
+                if (video !== null) {
+                    this.calculateVideoHeight(video);
+                }
+            });
+        });
+    };
+    calculateVideoHeight = video => {
+        /* Get the bounds of the video */
+        const videoWrapper = video.parentNode;
+
+        /* Initialize a new Player class and stop the video from playing */
+        const player = new Player(video);
+
+        /* Get video width and height */
+        Promise.all([player.getVideoWidth(), player.getVideoHeight()]).then(dimensions => {
+            const [width, height] = dimensions;
+            const paddingTop = `${(height / width) * 100}%`;
+
+            /* Set the calculated padding to the video bounds */
+            videoWrapper.style.padding = `${paddingTop} 0 0`;
+
+            this.grid.layout();
+        });
     };
 }
-
-// class Masonry {
-//     constructor(el) {
-//         this.DOM = { el: el };
-//         this.gridItems = this.DOM.el.getElementsByClassName("grid__item");
-
-//         /* Get cases observer to play videos on intersection */
-//         this.cases = document.getElementById("cases");
-//         this.casesObserver = document.getElementsByClassName("cases-observer")[0];
-//         this.casesObserver.__link = this.cases;
-//         this.casesIsPlaying = false;
-
-//         /* Get the grid object, its row-gap, and the size of its implicit rows */
-//         this.rowGap = parseInt(window.getComputedStyle(this.DOM.el).getPropertyValue("grid-row-gap"));
-//         this.rowHeight = parseInt(window.getComputedStyle(this.DOM.el).getPropertyValue("grid-auto-rows"));
-
-//         /* Wait for images to load en initialize events */
-//         this.imagesLoaded();
-//     }
-//     imagesLoaded() {
-//         imagesLoaded(this.DOM.el, () => {
-//             this.initEvents();
-//         });
-//     }
-//     initEvents() {
-//         /* Resize all the grid items on the load and resize events */
-//         const masonryEvents = ["load", "resize"];
-//         masonryEvents.forEach(event => {
-//             window.addEventListener(event, this.resizeAllMasonryItems());
-//         });
-
-//         /* Get video's and pauze them on load */
-//         document.querySelectorAll("[data-vimeo-player]").forEach(video => {
-//             const player = new Player(video);
-//             player.pause();
-//         });
-
-//         /* Initialize new Interestion observer */
-//         new IntersectionObserver(this.onIntersection, { threshold: 1 }).observe(this.casesObserver);
-//     }
-//     resizeAllMasonryItems() {
-//         /*
-//          * Loop through the grid items and execute the spanning function to
-//          * each list-item (i.e. each masonry item)
-//          */
-//         for (let index = 0; index < this.gridItems.length; index++) {
-//             this.resizeMasonryItem(this.gridItems[index]);
-//         }
-//     }
-//     resizeMasonryItem(item) {
-//         /* Specify which kind of masonry item it is and run specific function */
-//         if (item.classList.contains("grid__item--video") && window.innerWidth > 992) {
-//             this.resizeMasonryVideo(item);
-//         } else {
-//             this.resizeMasonryImage(item);
-//         }
-//     }
-//     resizeMasonryVideo(item) {
-//         /* Get the bounds of the video */
-//         const bounds = item.querySelector(".media__container");
-
-//         /* Initialize a new Player class and stop the video from playing */
-//         const player = new Player(bounds.children[0]);
-
-//         /* Get video width and height */
-//         Promise.all([player.getVideoWidth(), player.getVideoHeight()]).then(dimensions => {
-//             const [width, height] = dimensions;
-//             const paddingTop = `${(height / width) * 100}%`;
-
-//             /* Calculate the rowSpan */
-//             const rowSpan = Math.ceil(
-//                 (bounds.getBoundingClientRect().height + this.rowGap) / (this.rowHeight + this.rowGap)
-//             );
-
-//             /* Set the calculated padding to the video bounds */
-//             bounds.style.padding = `${paddingTop} 0 0`;
-
-//             /* Set the spanning as calculated above (S) */
-//             item.style.gridRowEnd = "span " + rowSpan;
-
-//             /* Make the video take all the available space in the cell/item */
-//             bounds.style.height = rowSpan * 5 - 5 + "px";
-//         });
-//     }
-//     resizeMasonryImage(item) {
-//         /* Get the bounds of the image */
-//         const bounds = item.querySelector(".img-fluid");
-
-//         if (bounds !== null) {
-//             /* Calculate the rowSpan */
-//             const rowSpan = Math.ceil(
-//                 (bounds.getBoundingClientRect().height + this.rowGap) / (this.rowHeight + this.rowGap)
-//             );
-
-//             /* Set the spanning as calculated above (S) */
-//             item.style.gridRowEnd = "span " + rowSpan;
-
-//             /* Make the images take all the available space in the cell/item */
-//             bounds.style.height = rowSpan * 5 + "px";
-//         }
-//     }
-//     onIntersection(entries) {
-//         if (this.casesIsPlaying) return;
-
-//         entries.forEach(entry => {
-//             if (entry.target.className === "cases-observer" && entry.intersectionRatio === 1) {
-//                 this.casesIsPlaying = true;
-
-//                 /* Get video's and play them on intersection */
-//                 document.querySelectorAll("[data-vimeo-player]").forEach(video => {
-//                     const player = new Player(video);
-//                     player.play();
-//                 });
-//             }
-//         });
-//     }
-// }
 
 const gridNode = document.querySelector(".row--masonry");
 if (gridNode !== null) {
