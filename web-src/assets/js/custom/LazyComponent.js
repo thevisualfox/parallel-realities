4e3a039e6d05ff6234b80afd3281708f400da137
Jurjen Vos <jurjen.vos@webstores.nl>
2020-01-05 18:44:29 +0100

    feat (work): animation optilmaisions

--- web-src/assets/js/custom/LazyComponent.js
+++ web-src/assets/js/custom/LazyComponent.js
@@ -1,97 +1,56 @@
 import gsap from "gsap";
 import Player from "@vimeo/player";
+import Layzr from "layzr.js";
 
-export class LazyImage {
-    constructor(images) {
-        this.DOM = { images };
-        this.observer = null;
+const lazyImages = Layzr({
+    threshold: 10
+});
 
-        this.initImageObserver();
-    }
-    initImageObserver = () => {
-        this.observer = new IntersectionObserver(
-            (images, observer) => {
-                images.forEach(image => {
-                    if (image.isIntersecting) {
-                        this.lazyLoadImage(image.target);
+document.addEventListener("DOMContentLoaded", () => {
+    lazyImages
+        .update()
+        .check()
+        .handlers(true);
+});
 
-                        observer.unobserve(image.target);
-                    }
-                });
-            },
-            { rootMargin: "0px 0px 800px 0px" }
-        );
+lazyImages.on("src:after", image => {
+    const parent = document.querySelector(image.dataset.parent);
+    const parentWrapper = parent.querySelector(".media__wrapper");
+    const parentContent = parent.querySelector(".media__link");
 
-        this.DOM.images.forEach(image => this.observer.observe(image));
-    };
-    lazyLoadImage = image => {
-        if (image !== null) {
-            image.src = image.dataset.src;
-            image.srcset = image.dataset.srcset;
-        }
-    };
-}
+    gsap.to([parent, parentWrapper], {
+        duration: 1,
+        translateY: 0,
+        ease: "circ.inOut"
+    });
+
+    gsap.to(parentContent, {
+        duration: 0.35,
+        scale: 1,
+        ease: "circ.inOut"
+    });
+});
 
 export class LazyVideo {
     constructor(videos) {
         this.DOM = { videos };
         this.observer = null;
 
-        this.initVideoObserver();
+        this.initEvents();
     }
-    initVideoObserver = () => {
-        this.observer = new IntersectionObserver(
-            videos => {
-                videos.forEach(video => {
-                    if (video.isIntersecting) {
-                        this.toggleVideoPlayer("play", video.target);
-                    } else {
-                        this.toggleVideoPlayer("pause", video.target);
-                    }
-                });
-            },
-            { rootMargin: "0px 0px 0px 0px" }
-        );
+    initEvents = () => {
+        this.DOM.videos.forEach(video => {
+            const videoNode = video.querySelector("[data-vimeo-player]");
 
-        this.DOM.videos.forEach(video => this.observer.observe(video));
-    };
-    toggleVideoPlayer = (action, video) => {
-        const videoPlayer = new Player(video);
+            if (videoNode !== null) {
+                const videoPlayer = new Player(videoNode);
 
-        action === "play" ? videoPlayer.play() : videoPlayer.pause();
-    };
-}
-
-export class LazyComponent {
-    constructor(components) {
-        this.DOM = { components };
-        this.observer = null;
-
-        this.initComponentObserver();
-    }
-    initComponentObserver = () => {
-        this.observer = new IntersectionObserver(
-            (components, observer) => {
-                components.forEach(component => {
-                    if (component.isIntersecting) {
-                        this.animateComponent(component.target);
-
-                        observer.unobserve(component.target);
-                    }
-                });
-            },
-            { rootMargin: "0px 0px 200px 0px" }
-        );
-
-        this.DOM.components.forEach(component => this.observer.observe(component));
-    };
-    animateComponent = component => {
-        gsap.to(component, {
-            duration: 0.75,
-            scale: 1,
-            opacity: 1,
-            translateY: 0,
-            ease: "circ.inOut"
+                video.addEventListener("mouseenter", () => this.toggleVideoPlayer("play", videoPlayer));
+                video.addEventListener("mouseleave", () => this.toggleVideoPlayer("pause", videoPlayer));
+            }
         });
+    };
+    toggleVideoPlayer = (action, videoPlayer) => {
+        action === "play" ? videoPlayer.play() : videoPlayer.pause();
     };
 }
--- web-src/assets/js/custom/masonry.js
+++ web-src/assets/js/custom/masonry.js
@@ -1,6 +1,5 @@
-import Masonry from "masonry-layout";
 import imagesLoaded from "imagesloaded";
-import { LazyImage, LazyComponent, LazyVideo } from "./LazyComponent";
+import { LazyVideo } from "./LazyComponent";
 import CalculateVideoHeight from "./CalculateVideoHeight";
 
 class MasonryGrid {
@@ -9,12 +8,6 @@ class MasonryGrid {
         this.DOM.videos = Array.from(this.DOM.el.querySelectorAll("[data-vimeo-player]"));
         this.DOM.images = Array.from(this.DOM.el.querySelectorAll(".img-lazy"));
         this.DOM.items = Array.from(this.DOM.el.querySelectorAll(".media"));
-
-        this.grid = new Masonry(this.DOM.el, {
-            itemSelector: ".row__item",
-            columnWidth: ".row__item",
-            percentPosition: true
-        });
 
         this.initMasonry();
     }
@@ -27,13 +20,9 @@ class MasonryGrid {
                     const status = await videoPlayers.editVideo(video, videoIndex);
 
                     if (status === "done") {
-                        this.grid.layout();
-
-                        this.initLazyTypes(["images", "components", "videos"]);
+                        this.initLazyTypes(["videos"]);
                     }
                 });
-            } else {
-                this.initLazyTypes(["images", "components"]);
             }
         });
     };
@@ -42,16 +31,8 @@ class MasonryGrid {
             const type = types[index];
 
             switch (type) {
-                case "images":
-                    new LazyImage(this.DOM.images);
-                    break;
                 case "videos":
-                    new LazyVideo(this.DOM.videos);
-                    break;
-                case "components":
-                    new LazyComponent(this.DOM.items);
-                    break;
-                default:
+                    new LazyVideo(this.DOM.items);
                     break;
             }
         }
